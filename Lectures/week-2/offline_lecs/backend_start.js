12b00e28c2d4550baeae54054b6074692d4ddbc4
Karan-Palan <karanpalan007@gmail.com>
2023-12-15 21:34:20 +0530

    week2-lec2_and_bash-basics

---
+++ Lectures/week-2/offline_lecs/backend_start.js
@@ -0,0 +1,29 @@
+// HTTP protocol
+/*
+1. Browser parses the URL
+2. Does a DNS Lookup (converts google.com to an IP)
+3. Establishes a connection to the IP (does handshake...)
+*/
+
+// Program for a hospital
+
+const express = require("express");
+const app = express;
+
+app.get("/", function(req,res) {
+    
+})
+
+app.post("/", function(req,res) {
+
+})
+
+app.put("/", function(req,res) {
+
+})
+
+app.patch("/", function(req, res) {
+
+})
+
+app.listen(3000);
