cd3b2ffe2724bb7dd064223bd640e252b98a8645
Karan-Palan <karanpalan007@gmail.com>
2023-12-22 11:39:26 +0530

    Promise APIs

--- Extras/PromiseAPI.js
+++ Extras/PromiseAPI.js
@@ -1,48 +1,48 @@
-// // Promise.all
+// Promise.all
 
-// const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("P1 Sucess"), 3000);
-// });
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P1 Sucess"), 3000);
+});
 
-// const p2 = new Promise((resolve, reject) => {
-//   //setTimeout(() => resolve("P2 Sucess"), 1000);
-//   setTimeout(() => reject("P2 failure"), 1000);
-// });
+const p2 = new Promise((resolve, reject) => {
+  //setTimeout(() => resolve("P2 Sucess"), 1000);
+  setTimeout(() => reject("P2 failure"), 1000);
+});
 
-// const p3 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("P3 Sucess"), 2000);
-// });
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P3 Sucess"), 2000);
+});
 
-// Promise.all([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+Promise.all([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-// // Promise.all Settled
+// Promise.all Settled
 
-// const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("P1 Sucess"), 3000);
-// });
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P1 Sucess"), 3000);
+});
 
-// const p2 = new Promise((resolve, reject) => {
-//   //setTimeout(() => resolve("P2 Sucess"), 1000);
-//   setTimeout(() => reject("P2 failure"), 1000);
-// });
+const p2 = new Promise((resolve, reject) => {
+  //setTimeout(() => resolve("P2 Sucess"), 1000);
+  setTimeout(() => reject("P2 failure"), 1000);
+});
 
-// const p3 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("P3 Sucess"), 2000);
-// });
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P3 Sucess"), 2000);
+});
 
-// Promise.allSettled([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+Promise.allSettled([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // Promise.any
 
@@ -64,4 +64,5 @@ Promise.any([p1, p2, p3]) //P2 sucess will be returned after 5 seconds
   })
   .catch((err) => {
     console.error(err);
+    console.log(err.errors); // An array of errors
   });
