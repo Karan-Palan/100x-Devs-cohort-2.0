7abe866f0983343ddf6171952431fa5a9267fdd7
Karan-Palan <karanpalan007@gmail.com>
2024-01-27 16:17:34 +0530

    UseCallback assignments done

--- Assignments/week-6/2-use-callback/src/components/Assignment1.jsx
+++ Assignments/week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,31 +1,44 @@
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    // Your code starts here
-    function handleIncrement() {
+  // Your code starts here
 
-    }
+  const memoisedHandleIncrement = useCallback(() => {
+    // Why did I add count as the dependency - If you are using a state variable , add it as a dependency
+    setCount(count + 1);
+    // Alter native better method - I wont have to add count as a dependency here 
+    /*
+    setCount(function(currentCount) {
+      currentCount = currentCount + 1;
+    })
+    */
+  }, [count]);
 
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
+    
+  const memoisedHandleDecrement = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
 
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
-        </div>
-    );
-};
+  // Your code ends here
 
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+  return (
     <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
+      <p>Count: {count}</p>
+      <CounterButtons
+        onIncrement={memoisedHandleIncrement}
+        onDecrement={memoisedHandleDecrement}
+      />
     </div>
-);
+  );
+}
+
+const CounterButtons = memo(({ onIncrement, onDecrement }) => (
+  <div>
+    <button onClick={onIncrement}>Increment</button>
+    <button onClick={onDecrement}>Decrement</button>
+  </div>
+));
