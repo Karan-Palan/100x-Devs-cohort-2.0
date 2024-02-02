3f6ef9755590e2b3816a9da1ca6278d0c3f2d8ca
Karan-Palan <karanpalan007@gmail.com>
2024-02-02 14:53:18 +0530

    Tailwind lec done

---
+++ Lectures/week-8/lec-1-tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { RevenueCard } from './components/RevenueCard'
+
+function App() {
+  return (
+    // Tailwind CSS is mobile first
+    // <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3' > 
+    //   <div>Hi there</div>
+    //   <div>Hi there</div>
+    //   <div>Hi there</div>
+    //   <div>Hi there</div>
+    // </div>  
+  )
+}
+
+export default App
