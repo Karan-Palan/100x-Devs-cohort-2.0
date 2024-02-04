3ceebe13495b1aec14307c6429d5e99daed629b7
Karan-Palan <karanpalan007@gmail.com>
2024-02-04 17:44:14 +0530

    Use Online hook done

--- Lectures/week-9/hooks_pracitce/src/App.jsx
+++ Lectures/week-9/hooks_pracitce/src/App.jsx
@@ -1,55 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function useTodos(n) {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
 
   useEffect(() => {
-    const value = setInterval(() => {
-      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      });
-    }, n * 1000);
+    window.addEventListener("online", () => setIsOnline(true));
+    window.addEventListener("offline", () => setIsOnline(false));
+  }, [])
+  return isOnline;
 
-    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-      setTodos(res.data.todos);
-      setLoading(false);
-    });
-
-    return () => {
-      clearInterval(value);
-    };
-  }, [n]);
-
-  return { todos, loading };
 }
 
 function App() {
-  const { todos, loading } = useTodos(10);
-
-  if (loading) {
-    return <div> loading... </div>;
+  const isOnline = useIsOnline();
+  if (isOnline) {
+    return <div>You are online</div>;
+  } else {
+    return <div>You are offline</div>;
   }
-
-  return (
-    <>
-      {todos.map((todo) => (
-        <Track todo={todo} />
-      ))}
-    </>
-  );
-}
-
-function Track({ todo }) {
-  return (
-    <div>
-      {todo.title}
-      <br />
-      {todo.description}
-    </div>
-  );
 }
 
 export default App;
