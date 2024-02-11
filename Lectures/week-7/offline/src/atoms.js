103067cb24efaab1526f742ddec971984e19e449
Karan-Palan <karanpalan007@gmail.com>
2024-02-11 12:50:58 +0530

    UseRecoilStateLoadable done

--- Lectures/week-7/offline/src/atoms.js
+++ Lectures/week-7/offline/src/atoms.js
@@ -1,9 +1,14 @@
-import { atomFamily } from "recoil";
-import { TODOS } from "./todos";
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
 
 export const todosAtomFamily = atomFamily({
   key: 'todosAtomFamily',
-  default: id => {
-    return TODOS.find(x => x.id === id)
-  },
+  default: selectorFamily({
+    key: "todoSelectorFamily",
+    get: (id) => async ({get}) => {
+      await new Promise(r => setTimeout(r, 5000));
+      const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+      return res.data.todo;
+    },
+  })
 });
